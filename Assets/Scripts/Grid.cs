fd1b767cdf3fec7fc43365e17ebe5110af8b7315
Jan Puto <tofukles@gmail.com>
2020-08-14 23:16:28 +0200

    Weights for terrain types

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -11,11 +11,17 @@ public class Grid : MonoBehaviour
     public Vector2 gridWorldSize;                                                   
     public float nodeRadius;    // Promień   
     public TerrainType[] walkableRegions;
+    public int obstacleProximityPenalty = 10;
     LayerMask walkableMask;
+    Dictionary<int, int> walkableRegionsDictionary = new Dictionary<int, int>();
+
     Node[,] grid;                                                                   
                                                                                     
     float nodeDiameter;     // Średnica                                             
-    int gridSizeX, gridSizeY;                                                       
+    int gridSizeX, gridSizeY;
+
+    int penaltyMax = int.MinValue;
+    int penaltyMin = int.MaxValue;
                    
     public int MaxSize
     {
@@ -30,10 +36,11 @@ public class Grid : MonoBehaviour
         nodeDiameter = nodeRadius * 2;                                              
         gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);               
         gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
-
+         
         foreach (TerrainType region in walkableRegions)
-        { 
-
+        {
+            walkableMask.value |= region.terrainMask.value;
+            walkableRegionsDictionary.Add(Mathf.RoundToInt(Mathf.Log(region.terrainMask.value,2)), region.terainPenalty);
         }
                                                                                     
         CreateGrid();                                                               
@@ -54,9 +61,83 @@ public class Grid : MonoBehaviour
 
                 int movementPenalty = 0;
 
-                // raycast 
+
+                Ray ray = new Ray(worldPoint + Vector3.up * 50, Vector3.down);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit, 100, walkableMask))
+                {
+                    walkableRegionsDictionary.TryGetValue(hit.collider.gameObject.layer, out movementPenalty);
+                    //Debug.Log(movementPenalty);
+                }
+
+                if (!walkable)
+                {
+                    movementPenalty += obstacleProximityPenalty;
+                }
 
                 grid[x, y] = new Node(walkable, worldPoint, x, y, movementPenalty);
+            }
+        }
+
+        BlurPenaltyMap(3);
+    }
+
+    void BlurPenaltyMap(int blurSize)                               // ni chuja nie mam pojęcia jak to działa https://www.youtube.com/watch?v=Tb-rM3wGwv4&list=PLFt_AvWsXl0cq5Umv3pMC9SPnKjfp9eGW&index=7
+    {
+        int kernelSize = blurSize * 2 + 1;
+        int kernelExtents = blurSize;                               // wielkość siatki do blurowania
+
+        int[,] penaltiesHorizontalPass = new int[gridSizeX, gridSizeY];
+        int[,] penaltiesVerticalPass = new int[gridSizeX, gridSizeY];
+
+        for (int y = 0; y < gridSizeY; y++)                         // Horizontal Pass
+        {
+            for (int x = -kernelExtents; x <= kernelExtents; x++)
+            {
+                int sampleX = Mathf.Clamp(x, 0, kernelExtents);
+                penaltiesHorizontalPass[0, y] += grid[sampleX, y].movementPenalty;
+            }
+
+            for(int x = 1; x < gridSizeX; x++)
+            {
+                int newNode = Mathf.Clamp(x - kernelExtents - 1, 0, gridSizeX);
+                int oldNode = Mathf.Clamp(x + kernelExtents, 0, gridSizeX - 1);
+
+                penaltiesHorizontalPass[x, y] = penaltiesHorizontalPass[x - 1, y] - grid[oldNode, y].movementPenalty + grid[newNode, y].movementPenalty;
+            }
+        }
+
+        for (int x = 0; x < gridSizeX; x++)                         // Vertical Pass
+        {
+            for (int y = -kernelExtents; y <= kernelExtents; y++)
+            {
+                int sampleY = Mathf.Clamp(y, 0, kernelExtents);
+                penaltiesVerticalPass[x, 0] += penaltiesHorizontalPass[x, sampleY];
+            }
+
+            int bluredPenalty = Mathf.RoundToInt((float)penaltiesVerticalPass[x, 0] / (kernelSize * kernelSize));
+            grid[x, 0].movementPenalty = bluredPenalty;
+
+            for (int y = 1; y < gridSizeY; y++)
+            {
+                int newNode = Mathf.Clamp(y - kernelExtents - 1, 0, gridSizeY);
+                int oldNode = Mathf.Clamp(y + kernelExtents, 0, gridSizeY - 1);
+
+                penaltiesVerticalPass[x, y] = penaltiesVerticalPass[x, y - 1] - penaltiesHorizontalPass[x, oldNode] + penaltiesHorizontalPass[x, newNode];
+                bluredPenalty = Mathf.RoundToInt((float)penaltiesVerticalPass[x,y] / (kernelSize * kernelSize));
+                grid[x, y].movementPenalty = bluredPenalty;
+
+                Debug.Log(bluredPenalty);
+
+                if (bluredPenalty > penaltyMax)                     // Gizmos
+                {
+                    penaltyMax = bluredPenalty;
+                }
+
+                if (bluredPenalty < penaltyMin)
+                {
+                    penaltyMin = bluredPenalty;
+                }
             }
         }
     }
@@ -108,8 +189,12 @@ public class Grid : MonoBehaviour
         {
             foreach (Node n in grid)
             {
-                Gizmos.color = (n.walkable) ? Color.green : Color.red;      // ? = THEN, : = ELSE
-                Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - .1f));
+
+                Gizmos.color = Color.Lerp(Color.white, Color.black, Mathf.InverseLerp(penaltyMin, penaltyMax, n.movementPenalty));
+                Gizmos.color = (n.walkable) ? Gizmos.color : Color.red;      // ? = THEN, : = ELSE
+                Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter));
+                // Gizmos.color = (n.walkable) ? Color.green : Color.red;      // ? = THEN, : = ELSE
+                // Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - .1f));
             }
         }
     }
